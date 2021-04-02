ef3dfe98701156b7ec73d98f5022f546260a316f
saikora- <c20027@std.it-college.ac.jp>
2021-04-02 17:03:59 +0900

    ○×ゲームを追加

---
+++ oxgame/Assets/Scripts/oxpoint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oxpoint : MonoBehaviour
+{
+    bool oxnum = true;
+    public GameObject[] oximage;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) oxdown();
+    }
+
+    public void oxdown()
+    {
+        if (oxnum){
+            Instantiate(
+                oximage[0],
+                Vector3.zero,
+                Quaternion.identity
+                );
+        }else{
+            Instantiate(
+                oximage[1],
+                Vector3.zero,
+                Quaternion.identity
+                );
+        }
+        oxnum = !oxnum;
+    }
+}
