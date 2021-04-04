8792a3b74eb592331ed894f5c6172fede7f2986b
saikora- <c20027@std.it-college.ac.jp>
2021-04-04 13:26:13 +0900

    とりあえず○×とだけ打てるように変更、ただしまだゲームとしての体裁は成していない

--- oxgame/Assets/Scripts/oxpoint.cs
+++ oxgame/Assets/Scripts/oxpoint.cs
@@ -1,32 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class oxpoint : MonoBehaviour
 {
     bool oxnum = true;
-    public GameObject[] oximage;
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) oxdown();
-    }
+    public Sprite[] oximage;
+
+    public Image button;
+
+    int num;
 
     public void oxdown()
     {
-        if (oxnum){
-            Instantiate(
-                oximage[0],
-                Vector3.zero,
-                Quaternion.identity
-                );
-        }else{
-            Instantiate(
-                oximage[1],
-                Vector3.zero,
-                Quaternion.identity
-                );
+        if (oxnum)
+        {
+            num = 0;
+        } else {
+            num = 1;
         }
+
+        button.sprite = oximage[num];
+
         oxnum = !oxnum;
     }
 }
